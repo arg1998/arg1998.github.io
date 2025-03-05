bf239744da9316304fdcc9c5f2d7a43e9a5e4e1c
ARGOSTA <arg7971@gmail.com>
2025-03-05 16:52:45 -0600

    updated nextjs configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", 
-  images: { unoptimized: true }, 
+  output: "export",
+  images: { unoptimized: true },
   trailingSlash: true,
-  eslint : {
-    ignoreDuringBuilds: true
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader"
+    });
+    return config;
   }
 };
 
