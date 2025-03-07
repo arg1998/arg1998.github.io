b2464b75d1e1cb0953ca9fcabcd05aee92fdd2a5
ARGOSTA <arg7971@gmail.com>
2025-03-06 19:14:22 -0600

    added style props to <BlogMarkdownContent />

--- src/components/BlogMarkdownContent.jsx
+++ src/components/BlogMarkdownContent.jsx
@@ -6,15 +6,14 @@ import remarkDirective from "remark-directive"
 import rehypeHighlight from "rehype-highlight";
 import rehypeRaw from "rehype-raw";
 import rehypeSlug from "rehype-slug"
-
 import rehypeKatex from "rehype-katex";
-import "katex/dist/katex.min.css";
 
+import "katex/dist/katex.min.css";
 import "highlight.js/styles/lioshi.css";
 
-const BlogMarkdownContent = ({ MarkdownFileContent }) => {
+const BlogMarkdownContent = ({ MarkdownFileContent, styles }) => {
   return (
-    <div className="blog-container-md">
+    <div className="blog-container-md" style={styles}>
       <ReactMarkdown
         remarkPlugins={[remarkGfm, remarkDirective, remarkMath, remarkObsidianCallout]}
         rehypePlugins={[rehypeRaw, rehypeHighlight, rehypeKatex, rehypeSlug]}
