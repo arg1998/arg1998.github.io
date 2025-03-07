616935a30b76005375df5e684850c1aa51119bab
ARGOSTA <arg7971@gmail.com>
2025-03-06 19:13:37 -0600

    added new ribbon

--- src/components/Ribbons.jsx
+++ src/components/Ribbons.jsx
@@ -1,10 +1,16 @@
 const RibbonWorInProgress = ({styles}) =>  {
-    return (<div className="ribbon ribbon-wip" style={styles}> <i className="bi bi-cone-striped" /> W.I.P </div>)
+    return (<div className="ribbon ribbon-wip" style={styles}> <i className="bi bi-cone-striped" /><span> W.I.P </span></div>)
+}
+
+const RibbonWorEducational = ({ styles }) => {
+    return (<div className="ribbon ribbon-educational" style={styles}> <i className="bi bi-mortarboard-fill" /> <span>Study</span> </div>)
 }
 
 
 
 
+
 export {
-    RibbonWorInProgress
+    RibbonWorInProgress,
+    RibbonWorEducational
 }
